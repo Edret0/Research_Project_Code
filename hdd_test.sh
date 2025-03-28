f8aea63d1a2b9fdb10dd296ad50696aea4eb4789
Edret <josejaviernavarro1@gmail.com>
2025-03-28 16:41:15 -0400

    Fixed script

---
+++ hdd_test.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash\
+
+
+sudo dd if=/dev/urandom of=/dev/sda bs=4k seek=1000 conv=fdatasync status=progress > dd_output.log 2>&1
+sudo dd if=/dev/urandom of=/dev/sdb bs=4k seek=1000 conv=fdatasync status=progress > dd_output.log 2>&1
+sudo dd if=/dev/urandom of=/dev/sdc bs=4k seek=1000 conv=fdatasync status=progress > dd_output.log 2>&1
+
+
+fio --name=latency-test --ioengine=sync --rw=randwrite --size=20G --bs=4k --numjobs=4 --runtime=60s --time_based  --output=fio_latency_results_sda.log --filename=/dev/sda
+fio --name=latency-test --ioengine=sync --rw=randwrite --size=20G --bs=4k --numjobs=4 --runtime=60s --time_based  --output=fio_latency_results_sdb.log --filename=/dev/sdb
+fio --name=latency-test --ioengine=sync --rw=randwrite --size=20G --bs=4k --numjobs=4 --runtime=60s --time_based  --output=fio_latency_results_sdc.log --filename=/dev/sdc
+
+fio --name=repeated-writes --ioengine=sync --rw=write --size=20G --bs=4k --numjobs=4 --runtime=5m --time_based --output=fio_repeated_writes_sda.log --filename=/dev/sda
+fio --name=repeated-writes --ioengine=sync --rw=write --size=20G --bs=4k --numjobs=4 --runtime=5m --time_based --output=fio_repeated_writes_sdb.log --filename=/dev/sdb
+fio --name=repeated-writes --ioengine=sync --rw=write --size=20G --bs=4k --numjobs=4 --runtime=5m --time_based --output=fio_repeated_writes_sdc.log --filename=/dev/sdc
+
+fio --name=fragmentation-test --ioengine=sync --rw=randwrite --size=20G --bs=4k --numjobs=8 --runtime=10m --time_based --output=fio_fragmentation_sda.log --filename=/dev/sda
+fio --name=fragmentation-test --ioengine=sync --rw=randwrite --size=20G --bs=4k --numjobs=8 --runtime=10m --time_based --output=fio_fragmentation_sdb.log --filename=/dev/sdb
+fio --name=fragmentation-test --ioengine=sync --rw=randwrite --size=20G --bs=4k --numjobs=8 --runtime=10m --time_based --output=fio_fragmentation_sdc.log --filename=/dev/sdc
